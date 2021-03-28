c49b252f98233647d42c6102c10461c75f740232
MisileLab <misileminecord@gmail.com>
2021-03-28 11:31:32 +0900

    Add CreateModpack

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ def install(package):
     else:
         pip._internal.main(['install', package])
 
-# Pip List
-install("PyQt5")
 # Code
 import sys
+try:
+    from PyQt5.QtWidgets import *
+    from PyQt5 import uic
+    from PyQt5.QtCore import Qt
+    from typing import Optional
+except ModuleNotFoundError:
+    install("PyQt5")
+    install("typing")
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
@@ -24,37 +30,40 @@ count = 0
 def btn_clicked():
     webbrowser.open("https://github.com/misilelab")
 
-def btn2_clicked():
-    global count
-    if count < 3:
-        webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-        count = count + 1
-    else:
-        webbrowser.open("http://warning.or.kr/")
+def Notice(self, modpackname:str):
+    QMessageBox.question(
+        self, 'Modpack Installer', f"{modpackname} Installed",
+        QMessageBox.Ok
+    )
 
 modpack = ""
+path = os.getcwd()
 
-# noinspection PyGlobalUndefined
+def deletetest(modfolder:str, repo:str, branch:str="main"):
+    path1 = os.getcwd()
+    repounzip = f"{repo}-{branch}"
+    repozip = f"{repo}.zip"
+    try:
+        shutil.rmtree(rf"{path1}\{modfolder}")
+        os.remove(repozip)
+        shutil.rmtree(rf"{path1}\{repounzip}")
+    except FileNotFoundError:
+        raise FileNotFoundError
+
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         global modpack
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(btn_clicked)
-        self.pushButton_2.clicked.connect(btn2_clicked)
         self.setWindowTitle('QCheckBox')
         self.pushButton_3.clicked.connect(self.btn3_clicked)
-        self.CreativeCraftPlus = QCheckBox("CreativeCraft+", self)
-        self.CreativeCraftPlus.move(10, 10)
-        self.BasicCraft = QCheckBox("BasicCraft", self)
-        self.BasicCraft.move(10, 30)
     def btn3_clicked(self):
-        print("1234")
         if self.CreativeCraftPlus.isChecked():
             try:
-                os.rmdir("CreativeCraft+ Mods")
+                shutil.rmtree(path + "\CreativeCraft+ Mods")
                 os.remove("CreativeCraft+.zip")
-                os.rmdir("CreativeCraft--main")
+                shutil.rmtree(path + "\CreativeCraft--main")
             except FileNotFoundError or FileExistsError:
                 pass
             os.system("curl https://codeload.github.com/MisileLab/CreativeCraft-/zip/main > CreativeCraft+.zip")
@@ -62,25 +71,38 @@ class MyWindow(QMainWindow, form_class):
             os.remove("CreativeCraft+.zip")
             os.makedirs("CreativeCraft+ Mods")
             shutil.move("CreativeCraft--main\overrides\mods", "CreativeCraft+ Mods")
-            os.rmdir("CreativeCraft--main")
+            shutil.rmtree(path + "\CreativeCraft--main")
+            Notice(self, modpackname="CreativeCraft+")
         if self.BasicCraft.isChecked():
             try:
-                os.rmdir("BasicCraft Mods")
+                shutil.rmtree(path + "\BasicCraft Mods")
                 os.remove("BasicCraft.zip")
-                os.rmdir("Basiccraft-main")
+                shutil.rmtree(path + "\Basiccraft-main")
             except FileNotFoundError or FileExistsError:
                 pass
-                print("2130")
             os.system("curl https://codeload.github.com/MisileLab/basiccraft/zip/main > BasicCraft.zip")
             os.system("unzip BasicCraft.zip")
             os.remove("BasicCraft.zip")
             os.makedirs("BasicCraft Mods")
             if self.plainTextEdit.toPlainText() == "1.16.5":
                 shutil.move("Basiccraft-main\\1.16.5", "BasicCraft Mods")
-                os.rmdir("Basiccraft-main")
             elif self.plainTextEdit.toPlainText() == "1.16.4":
                 shutil.move("Basiccraft-main\\1.16.4", "BasicCraft Mods")
-                os.rmdir("Basiccraft-main")
+            shutil.rmtree(path + r"\basiccraft-main")
+            Notice(self, modpackname="BasicCraft")
+        if self.CreateModpack.isChecked():
+            try:
+                deletetest(modfolder="CreateModpack mods", repo="CreateModpack")
+            except FileNotFoundError:
+                pass
+            os.system("curl https://codeload.github.com/MisileLab/CreateModpack/zip/main > CreateModpack.zip")
+            os.system("unzip CreateModpack.zip")
+            os.remove("CreateModpack.zip")
+            os.makedirs("CreateModpack mods")
+            shutil.move("CreateModpack-main\overrides\mods", "CreateModpack mods")
+            shutil.rmtree(path + "\CreateModpack-main")
+            Notice(self, modpackname="CreateModpack")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
